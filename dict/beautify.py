a9c42e1ed42c042aa2c802ec3f5c8499339d38f8
Hyunsik Jeong <rbtree@postech.ac.kr>
2018-11-11 02:14:23 +0900

    Add beautify code for dictionary management

---
+++ dict/beautify.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+for file in os.listdir():
+    if file.endswith('.json'):
+        print(file)
+        data = json.load(open(file, 'r', encoding='utf8'))
+
+        if file == 'doll_dict.json':
+            for num in data:
+                data[num]['alias'] = sorted(data[num]['alias'])
+        json.dump(data, open(file, 'w', encoding='utf8'), ensure_ascii=False, indent=4, sort_keys=True)
